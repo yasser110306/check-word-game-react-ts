658cb1f5e059b262539270b2b0af1e7b371b778c
yasser110306 <yasserrabie110306@gmail.com>
2024-02-12 19:05:07 +0200

    game logic done.

--- src/components/PlayZone.tsx
+++ src/components/PlayZone.tsx
@@ -6,40 +6,66 @@ import { useEffect, useState } from "react";
 const answer = getRandomAnswer();
 
 function PlayZone(): JSX.Element {
-    // updates the number of the try selected in the query.
-    const [tryNumber, setTryNumber] = useState(1);
+    // state if the user won or lost
+    const [gameResult, setGameResult] = useState<boolean>(false);
 
-    // updates the active inputs adn pass it to th echeck function.
-    const [answerFields, setAnswerFields] = useState<Element[] | null>(null);
+    // state the number of the current try for the selector.
+    const [tryNumber, setTryNumber] = useState<number>(1);
+
+    // state if the user ended his tries.
+    const [curTry, setCurTry] = useState<number>(1);
+
+    // updates the active inputs and pass it to the check function.
+    const [inputs, setInputs] = useState<Element[] | null>(null);
 
     // updates the state only when the check btn is clicked.
     useEffect(() => {
         // updating exictance of the active input.
-        const inputs = document.querySelectorAll(
+        const answerFields = document.querySelectorAll(
             `.try[data-trynum="${tryNumber.toString()}"] input `
         ) as NodeListOf<HTMLInputElement>;
 
         //updating the inputs in the state.
-        setAnswerFields(Array.from(inputs));
+        setInputs(Array.from(answerFields));
 
-        // focusing in the first input every try
-        inputs[0].focus();
+        // focusing in the first input every try.
+        answerFields[0].focus();
 
         // adding active class so it can be easily accessed.
-        inputs.forEach((input) => {
+        answerFields.forEach((input) => {
+            input.setAttribute("maxlength", "1");
             input.classList.add("active");
             input.removeAttribute("readOnly");
         });
 
         // updating exictance of the unactive input.
         const notActiveInputs = document.querySelectorAll("input:not(.active)");
-        
+
         // updating the unactive inputs attributes.
         notActiveInputs?.forEach((input) => {
             input.setAttribute("maxlength", "1");
             input.setAttribute("readOnly", "true");
         });
     }, [tryNumber]);
+
+    // click event function.
+    const handleCheck = () => {
+        // cheecks if the user stil have tries.
+        if (curTry < 5) {
+            // storing the returned data from check inputs function.
+            const userState = CheckInputs({ inputs, answer });
+
+            // updating the states  of the current try and user game result.
+            setGameResult(userState.userState);
+            setCurTry(userState.curTry);
+        }
+
+        // go to next try.
+        if (tryNumber < 5) {
+            setTryNumber(tryNumber + 1);
+        }
+    };
+
     return (
         <div className="play-zone">
             <div className="tries">
@@ -49,16 +75,9 @@ function PlayZone(): JSX.Element {
                 <Try trynum="4" />
                 <Try trynum="5" />
             </div>
-            <button
-                onClick={() => {
-                    CheckInputs(answerFields, answer);
-                    if (tryNumber !== 5) {
-                        setTryNumber(tryNumber + 1);
-                    }
-                }}
-            >
-                Check Word
-            </button>
+            <button onClick={handleCheck}>Check Word</button>
+            {gameResult && <p className="state-alert">Congratulations! You won!</p>}
+            {curTry === 5 && !gameResult && <p className="state-alert">Sorry, you lost!</p>}
         </div>
     );
 }
--- src/functions/GameLogic.tsx
+++ src/functions/GameLogic.tsx
@@ -17,18 +17,75 @@ const answers: string[] = [
     "PIZZA",
 ];
 
+// type of data coming from the play zone component.
+interface Data {
+    inputs: Element[] | null;
+    answer: string[];
+}
+
 function getRandomAnswer(): string[] {
     const answerIndex = Math.floor(Math.random() * answers.length);
     const answer: string = answers[answerIndex];
     const solution: string[] = answer.split("");
     return solution;
 }
-// still unfinished...
-function CheckInputs(inputs: Element[] | null, answer: string[]): number {
+
+interface dataToGame {
+    userState: boolean;
+    curTry: number;
+}
+
+// declares the try the user is in.
+let curTry: number = 0;
+
+// checks the inputs and judge if they are right or wrong.
+function CheckInputs({ inputs, answer }: Data): dataToGame {
+    // makes the inputs un accessable after checking.
     inputs?.forEach((input) => {
         input.classList.remove("active");
     });
+
+    // counter of number of in place letters.
+    let score: number = 0;
+
+    inputs?.forEach((input) => {
+        // make sure that the inputs is not null.
+        if (input instanceof HTMLInputElement) {
+            const userInput: string = input.value.toUpperCase();
+
+            // check if the letter user enterd exist on the answer.
+            if (answer.includes(userInput)) {
+                // check if the letter is in place.
+                if (inputs.indexOf(input) === answer.indexOf(userInput)) {
+                    input.classList.add("in-place");
+                    score++;
+                } else {
+                    input.classList.add("not-in-place");
+                }
+            } else {
+                input.classList.add("wrong");
+            }
+        }
+    });
+
+    // update the number of the current try in the state.
+    curTry++;
+
+    // check if the user won and return a boolean.
+    const userState: boolean = handleLoseOrWin(score, answer);
+
+    return { userState, curTry };
 }
+
+const handleLoseOrWin = (score: number, answer: string[]): boolean => {
+    let state: boolean = false;
+    // check if user has guessed all the letters.
+    if (score === answer.length) {
+        state = true;
+    }
+
+    return state;
+};
 
 // eslint-disable-next-line react-refresh/only-export-components
 export { CheckInputs, getRandomAnswer };
